f2ee03bd6f09f4794ff12e752055e8e144c75659
eikur <jorge.soriano.pinedo@gmail.com>
2017-02-04 20:25:48 +0100

    Updated folder structure
    Also added scripts for no-rigidbody required move

---
+++ Metalhalla/Assets/Scripts/PlayerMove - No Rigidbody/myPlayerController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(myPlayerInput))]
+[RequireComponent (typeof(myPlayerStatus))]
+[RequireComponent (typeof(myPlayerMove))]
+[RequireComponent (typeof(myPlayerCollider))]
+
+
+public class myPlayerController : MonoBehaviour {
+
+	myPlayerInput 	 playerInput;
+	myPlayerStatus   playerStatus;
+	myPlayerMove	 playerMove;
+	myPlayerCollider playerCollider;
+	Animator		 playerAnimator;
+
+	void Start() {
+		playerInput = GetComponent<myPlayerInput> ();
+		playerStatus = GetComponent<myPlayerStatus> ();
+		playerMove = GetComponent<myPlayerMove> ();
+		playerCollider = GetComponent<myPlayerCollider> ();
+		playerAnimator = GetComponent<Animator> ();
+	}
+
+	void Update () {
+		playerInput.GetInput ();
+	}
+	void FixedUpdate (){
+		// faltará actualizar el estado
+		//playerInput.GetInput ();
+		playerStatus.statusUpdateAfterInput(playerInput);
+		playerMove.CalculateSpeed(playerInput,playerStatus);
+		playerCollider.CheckMove (ref playerMove);
+		playerStatus.statusUpdateAfterCollisionCheck (playerCollider);
+		playerMove.Move ();
+		// send the animator what needs to be sent :)
+		playerAnimator.SetBool("inAir",!playerStatus.newStatus.IsGround());
+		playerAnimator.SetFloat("speed",Mathf.Abs(playerMove.speed.x));
+
+	}
+
+
+
+
+
+
+
+
+}
---
+++ Metalhalla/Assets/Scripts/PlayerMove - No Rigidbody/myPlayerInteractor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class myPlayerInteractor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void HitFromBelow (RaycastHit2D hit){
+			hit.transform.gameObject.SendMessage ("HitFromBelow", SendMessageOptions.DontRequireReceiver);
+	}
+}
