0adfaaf6e1f290e6ef1667342baea06654fffe13
Daniel Pérez <dperezparaiso@gmail.com>
2017-02-15 22:19:31 +0100

    M4_CODE_1: Transition Title to Game Menu using animations

---
+++ Metalhalla/Assets/Scripts/TransitionTitleMenuAnimation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransitionTitleMenuAnimation : MonoBehaviour {
+
+    public string sceneName;
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
