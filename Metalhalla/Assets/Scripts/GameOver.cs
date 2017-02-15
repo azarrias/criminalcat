87c900b1cb70053ca7e296688b43447cea11e291
Daniel Pérez <dperezparaiso@gmail.com>
2017-02-15 14:27:03 +0100

    [M4_CODE_7] Transition ingame to game over

---
+++ Metalhalla/Assets/Scripts/GameOver.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour {
+
+    public string nextScene;
+    public Button mainMenu;
+
+	// Use this for initialization
+	void Start () {
+
+        mainMenu = mainMenu.GetComponent<Button>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void MainMenuPressed()
+    {
+        SceneManager.LoadScene(nextScene);
+    }
+
+    
+
+
+}
---
+++ Metalhalla/Assets/Scripts/GameOverTransition.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverTransition : MonoBehaviour {
+
+    public GameObject gameOverUI;
+
+	// Use this for initialization
+	void Start () {
+        gameOverUI.SetActive(false);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            gameOverUI.SetActive(true);
+        }
+	}
+}
