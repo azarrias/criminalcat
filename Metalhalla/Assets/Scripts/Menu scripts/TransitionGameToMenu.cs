b1b373715e893c660422dbf8a48561f9736b641f
Daniel Pérez <dperezparaiso@gmail.com>
2017-02-18 19:35:11 +0100

    Fix highlight in button in transition from Game to GameMenu
    Resume button is highlighted properly

--- Metalhalla/Assets/Scripts/Menu scripts/TransitionGameToMenu.cs
+++ Metalhalla/Assets/Scripts/Menu scripts/TransitionGameToMenu.cs
@@ -2,21 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class TransitionGameToMenu : MonoBehaviour {
 
     public GameObject menu;
     public GameObject gameOverUI;
-    public GameObject resume;
-    public GameObject newGame;
-
-    private bool isIngameMenu;
-   
+     
 	// Use this for initialization
 	void Start () {
 
         menu.SetActive(false);
-        isIngameMenu = menu.GetComponent<MenuScript>().isIngameMenu;
 
     }
 	
@@ -36,15 +32,14 @@ public class TransitionGameToMenu : MonoBehaviour {
             else
             {
                 menu.SetActive(true);
-                
-                //Set the default selected menu option -> hay que usar tags, si no, no los pilla
-                if (isIngameMenu)
-                    EventSystem.current.SetSelectedGameObject(resume);
-                else
-                    EventSystem.current.SetSelectedGameObject(newGame);
 
                 //Game paused
                 Time.timeScale = 0f;
+                //Re-select the button in order to highlight it again
+                EventSystem.current.SetSelectedGameObject(null);
+                GameObject resume = GameObject.FindGameObjectWithTag("GameMenuResume");
+                EventSystem.current.SetSelectedGameObject(resume);
+                
             }
         }   
 	}
