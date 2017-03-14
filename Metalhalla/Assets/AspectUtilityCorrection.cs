13d00d8e90426b73536ee1dbf85ebbac84930830
eikur <jorge.soriano.pinedo@gmail.com>
2017-03-14 12:10:31 +0100

    AspectUtility scripts created: force 16:9 aspect ratio and add letterboxing or pillarboxing as needed

---
+++ Metalhalla/Assets/AspectUtilityCorrection.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AspectUtilityCorrection : MonoBehaviour {
+
+    RectTransform rect; 
+	// Use this for initialization
+	void Start () {
+        rect = GetComponent<RectTransform>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        rect.anchoredPosition = new Vector2(AspectUtility.xOffset, AspectUtility.yOffset);
+    }
+ 
+}
