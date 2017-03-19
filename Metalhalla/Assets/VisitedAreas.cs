269709a2de78e9811bc59dba902b0d96a5f9a86a
azarrias <adolfo.zarrias@gmail.com>
2017-03-20 00:02:13 +0100

    First draft of MiniMap

---
+++ Metalhalla/Assets/VisitedAreas.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisitedAreas : MonoBehaviour {
+
+    private GameObject player;
+    private List<GameObject> tiles = new List<GameObject>();
+    private Color redColor = new Color(255, 0, 0);
+
+    void Start () {
+        foreach (Transform t in this.gameObject.transform)
+        {
+            if (t.gameObject.layer == LayerMask.NameToLayer("map"))
+            {
+                tiles.Add(t.gameObject);
+            }
+        }
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        foreach(GameObject go in tiles)
+        {
+            if (player.transform.position.x > go.transform.position.x - go.transform.lossyScale.x &&
+                player.transform.position.x < go.transform.position.x + go.transform.lossyScale.x &&
+                player.transform.position.y > go.transform.position.y - go.transform.lossyScale.y &&
+                player.transform.position.y < go.transform.position.y + go.transform.lossyScale.y)
+            {
+                go.GetComponent<Renderer>().material.color = redColor;
+            }
+        }
+	}
+}
