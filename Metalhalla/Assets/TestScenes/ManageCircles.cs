15429eb219b0ba7211b579bf46141c356a5553d7
Daniel Pérez <dperezparaiso@gmail.com>
2017-04-13 18:48:47 +0200

    Added eagle attack (tornado) prefab

---
+++ Metalhalla/Assets/TestScenes/ManageCircles.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManageCircles : MonoBehaviour {
+
+    private  GameObject[] tornadoCircles;
+    private bool wait = false;
+    private int index = 0;
+    private float waitingTime = 0.3f;
+    public int circleAmount = 5;
+    public GameObject tornadoCircle = null;
+
+	// Use this for initialization
+	void Start () {
+
+        tornadoCircles = new GameObject[circleAmount];
+
+        for (int i = 0; i < circleAmount; i++)
+        {
+            tornadoCircles[i] = Instantiate<GameObject>(tornadoCircle, transform.position, Quaternion.identity);
+            tornadoCircles[i].transform.parent = transform;
+        }
+     
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (!wait)
+        {
+            tornadoCircles[index].GetComponent<TornadoCircleBehaviour>().readyToMove = true;
+            index++;
+            if (index >= tornadoCircles.Length)
+            {
+                index = 0;
+            }
+
+            wait = true;
+            StartCoroutine("WaitMyTurn", waitingTime);
+        }
+
+    }
+
+    IEnumerator WaitMyTurn(float seconds)
+    {      
+        Debug.Log("wait value=" + wait);
+        yield return new WaitForSeconds(seconds);
+        wait = false;        
+    }
+}
---
+++ Metalhalla/Assets/TestScenes/TornadoCircleBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TornadoCircleBehaviour : MonoBehaviour {
+
+    public float maxHeight = 2.0f;
+    public float speed = 2.0f;
+    public bool readyToMove = false;
+    
+
+    // Use this for initialization
+    void Start()
+    {
+        transform.localPosition = Vector3.zero;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (readyToMove)
+        {
+            Vector3 newPos = new Vector3();
+            newPos = transform.position + Vector3.up * speed * Time.deltaTime;
+            transform.position = newPos;
+
+            if (transform.localPosition.y >= maxHeight)
+            {
+                transform.localPosition = Vector3.zero;
+                readyToMove = false;            
+            }
+        }
+        
+	}
+}
