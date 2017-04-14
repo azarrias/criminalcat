6db4cd7cf16e41eda477ad3d832670c118000ad2
Daniel Pérez <dperezparaiso@gmail.com>
2017-04-14 18:29:43 +0200

    Added rockfall

---
+++ Metalhalla/Assets/Scripts/Miscellaneous scripts/RockBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockBehaviour : MonoBehaviour {
+
+    public int damage = 1;
+    public float lifeTime = 3.0f;
+	// Use this for initialization
+	void Start () {
+
+        StartCoroutine("DestroyRock", lifeTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnColissionEnter(Collision collision)
+    {
+        
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.SendMessage("ApplyDamage", damage);
+        }
+
+    }
+
+    private IEnumerator DestroyRock(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        //Crear algún efecto de polvo o algo con sistemas de partículas
+        Destroy(gameObject, 0.5f);
+    }
+}
---
+++ Metalhalla/Assets/Scripts/Miscellaneous scripts/RockFall.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class RockFall : MonoBehaviour {
+
+    public GameObject spawnPoint1 = null;
+    public GameObject spawnPoint2 = null;
+    public GameObject spawnPoint3 = null;
+    public GameObject rockPrefab = null;
+    private Transform currentSpawnPoint = null;
+    public float spawnDelay = 2.0f;
+    private bool wait = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!wait)
+        {
+            SelectSpawnPoint();
+            StartCoroutine("Delay", spawnDelay);
+        }
+
+    }
+
+    private void SelectSpawnPoint()
+    {
+        System.Random rand = new System.Random();
+        int num = rand.Next(0, 3);
+
+        switch(num)
+        {
+            case 0:
+                SpawnRock(spawnPoint1);
+                break;
+            case 1:
+                SpawnRock(spawnPoint2);
+                break;
+            case 2:
+                SpawnRock(spawnPoint3);
+                break;
+        }
+
+    }
+
+    private void SpawnRock(GameObject sp)
+    {
+        wait = true;
+        GameObject rock = Instantiate(rockPrefab, sp.transform.position, Quaternion.identity);
+        rock.transform.parent = transform;
+    }
+
+    private IEnumerator Delay(float seconds)
+    {
+        System.Random rand = new System.Random();
+        double randomTime = rand.NextDouble();
+        yield return new WaitForSeconds(seconds + (float)randomTime);
+        wait = false;
+    }
+}
