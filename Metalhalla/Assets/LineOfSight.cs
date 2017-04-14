47873d6883e40767e2afbbb693d394e8b428b1d5
azarrias <adolfo.zarrias@gmail.com>
2017-04-14 18:06:14 +0200

    Line of sight implemented for enemies

---
+++ Metalhalla/Assets/LineOfSight.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class LineOfSight : MonoBehaviour
+{
+
+    public float viewRadius;
+    [Range(0, 360)]
+    public float viewAngle;
+
+    public LayerMask obstacleMask;
+    private GameObject player;
+
+    public void Awake()
+    {
+
+    }
+
+    public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal)
+    {
+        if (!angleIsGlobal)
+        {
+            angleInDegrees += transform.eulerAngles.y;
+        }
+        return new Vector3(-Mathf.Cos(angleInDegrees * Mathf.Deg2Rad), -Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0);
+    }
+}
