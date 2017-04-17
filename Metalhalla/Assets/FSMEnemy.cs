ca3afb4a6bb90caaec68153330218cf52201566c
azarrias <adolfo.zarrias@gmail.com>
2017-04-17 12:33:12 +0200

    Add Stunned state to enemy fsm
    When an enemy is stunned, its LineOfSight is deactivated

--- Metalhalla/Assets/FSMEnemy.cs
+++ Metalhalla/Assets/FSMEnemy.cs
@@ -10,15 +10,19 @@ public class FSMEnemy : MonoBehaviour
     public LineOfSight los;
     [HideInInspector]
     public State state;
-    private float nextLocation = 0;
+
     [Tooltip("X world coordinate to be set as a left limit for this enemy")]
     public int leftPatrolLimit;
     [Tooltip("X world coordinate to be set as a right limit for this enemy")]
     public int rightPatrolLimit;
     public float speed = 1.0f;
 
+    public bool stunned = false;
+    private float nextLocation = 0;
+
     public enum State
     {
+        Stunned,
         Patrol,
         Chase,
         Attack
@@ -52,13 +56,20 @@ public class FSMEnemy : MonoBehaviour
         while (inPatrol)
         {
             transform.Translate(Vector3.left * Time.deltaTime * speed);
-            if (los.playerInSight)
+            if (stunned || los.playerInSight)
             {
                 inPatrol = false;
             }
             yield return null;
         }
-        state = State.Chase;
+        if (stunned)
+        {
+            state = State.Stunned;
+        }
+        else
+        {
+            state = State.Chase;
+        }
     }
 
     IEnumerator Chase()
@@ -69,12 +80,42 @@ public class FSMEnemy : MonoBehaviour
         while (inChase)
         {
             transform.Translate(Vector3.left * Time.deltaTime * speed * 2);
-            if (!los.playerInSight)
+            if (stunned || !los.playerInSight)
             {
                 inChase = false;
             }
             yield return null;
         }
+        if (stunned)
+        {
+            state = State.Stunned;
+        }
+        else
+        {
+            state = State.Patrol;
+        }
+    }
+
+    IEnumerator Stunned()
+    {
+        Debug.Log(name.ToString() + ": I'm stunned");
+        yield return null;
+        while (stunned)
+        {
+            yield return new WaitForSeconds(2.0f);
+        }
         state = State.Patrol;
+    }
+
+    public void Stun()
+    {
+        stunned = true;
+        los.Stop();
+    }
+
+    public void WakeUp()
+    {
+        stunned = false;
+        los.Start();
     }
 }
