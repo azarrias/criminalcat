0d345638b5faf91430b76a610903b506fa6e754b
eikur <jorge.soriano.pinedo@gmail.com>
2017-04-22 23:22:54 +0200

    Added a chest prefab to prepare for the chest animation and get of contents

---
+++ Metalhalla/Assets/Scripts/Miscellaneous scripts/openChestController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class openChestController : MonoBehaviour
+{
+
+    public GameObject content;
+    public Vector3 contentPositionOffset = new Vector3(0, 1, 0);
+
+    enum state { CLOSED, OPEN };
+    state currentState = state.CLOSED;
+
+    private void Start()
+    {
+        currentState = state.CLOSED;
+    }
+
+    // when attacking the chest it gets open
+    public void ApplyDamage(int dmg = 0)
+    {
+        if (currentState == state.CLOSED)
+        {
+            currentState = state.OPEN;
+            // start the open animation
+            /**/
+            // instance the contents
+            if (content != null)
+                Instantiate(content, contentPositionOffset, content.transform.rotation);
+        }
+
+    }
+}
