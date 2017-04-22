0f0b6ab0b813e052c7204030f804e91b587d2614
eikur <jorge.soriano.pinedo@gmail.com>
2017-04-22 10:11:39 +0200

    Added ShowDebugCollider script to player and mapped togle to P key

---
+++ Metalhalla/Assets/Scripts/Miscellaneous scripts/ShowDebugCollider.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowDebugCollider : MonoBehaviour {
+
+    public bool showCollider = false;
+    private Renderer colliderRenderer;
+
+    // Update is called once per frame
+
+    private void Start()
+    {
+        colliderRenderer = GetComponent<Renderer>();
+        colliderRenderer.enabled = showCollider;
+    }
+
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.P) == true)
+            ToggleVisibility();
+        
+    }
+
+    void ToggleVisibility()
+    {
+        showCollider = !showCollider;
+        colliderRenderer.enabled = showCollider;
+    }
+}
