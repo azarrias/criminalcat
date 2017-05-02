b15cef4434abc22eb36aab822b18ef6ce526834c
Daniel Pérez <dperezparaiso@gmail.com>
2017-05-02 15:14:58 +0200

    Added particles for the wild boar attack

---
+++ Metalhalla/Assets/Particles Systems/Scripts/WildBoar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WildBoar : MonoBehaviour {
+
+    public float speed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        gameObject.transform.position += Vector3.right * speed * Time.deltaTime;
+
+	}
+}
