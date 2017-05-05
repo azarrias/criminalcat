c2fa82b6518a04516d0633a6255c57a2e6bce0dd
Daniel Pérez <dperezparaiso@gmail.com>
2017-05-05 17:07:05 +0200

    Added Wildboar prefab created

--- Metalhalla/Assets/Particles Systems/Scripts/WildBoar.cs
+++ Metalhalla/Assets/Particles Systems/Scripts/WildBoar.cs
@@ -6,15 +6,59 @@ public class WildBoar : MonoBehaviour {
 
     public float speed = 1.0f;
 
+    public GameObject trail;
+    public GameObject dust;
+    public GameObject stoneEmitter;
+
+    private ParticleSystem trailParticles;
+    private ParticleSystem dustParticles;
+    private ParticleSystem stoneParticles;
+    private Vector3 moveDirection = Vector3.right; //debug test
+    private bool stop = false;
+
+
+    void Awake()
+    {
+        trailParticles = trail.GetComponent<ParticleSystem>();
+        dustParticles = dust.GetComponent<ParticleSystem>();   
+        stoneParticles = stoneEmitter.GetComponent<ParticleSystem>();
+
+
+    }
+
 	// Use this for initialization
-	void Start () {
-		
-	}
+	void Start () {       
+
+        trailParticles.Play();
+        dustParticles.Stop();
+        stoneParticles.Stop();
+    }
 	
 	// Update is called once per frame
 	void Update () {
 
-        gameObject.transform.position += Vector3.right * speed * Time.deltaTime;
-
+       if(!stop)
+        {
+            MoveParticleSystem(moveDirection);
+        }    
 	}
+
+    void MoveParticleSystem(Vector3 direction)
+    {
+        gameObject.transform.position += direction * speed * Time.deltaTime;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        trailParticles.Stop();
+        dustParticles.Play();
+        stoneParticles.Play();
+        stop = true;
+    }
+
+    public void SetMoveDirection(Vector3 direction)
+    {
+        moveDirection = direction;
+    }
 }
