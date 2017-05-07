8376d71f7c4152a7eb8609aa0662aa8ab687d993
azarrias <adolfo.zarrias@gmail.com>
2017-05-07 19:11:25 +0200

    FSMEnemy - Enable ZWrite

--- Metalhalla/Assets/FSMEnemy.cs
+++ Metalhalla/Assets/FSMEnemy.cs
@@ -263,7 +263,7 @@ public class FSMEnemy : MonoBehaviour
 
         material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
         material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
-        material.SetInt("_ZWrite", 0);
+        material.SetInt("_ZWrite", 1);
         material.DisableKeyword("_ALPHATEST_ON");
         material.EnableKeyword("_ALPHABLEND_ON");
         material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
