0b6f416b5c7d6e7bcf594e8b2929f2fda86f601f
Daniel Pérez <dperezparaiso@gmail.com>
2017-05-07 22:14:08 +0200

    Added interaction between wildboar particles and wildboar wall

--- Metalhalla/Assets/Particles Systems/Scripts/WildBoar.cs
+++ Metalhalla/Assets/Particles Systems/Scripts/WildBoar.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WildBoar : MonoBehaviour {
+public class WildBoar : MonoBehaviour
+{
 
     public float speed = 1.0f;
 
@@ -15,33 +16,39 @@ public class WildBoar : MonoBehaviour {
     private ParticleSystem stoneParticles;
     private Vector3 moveDirection = Vector3.right; //debug test
     private bool stop = false;
+    private bool collision = false;
+    public float lifeTime = 2.0f;
+    public float timeToDeactivate;
 
 
     void Awake()
     {
         trailParticles = trail.GetComponent<ParticleSystem>();
-        dustParticles = dust.GetComponent<ParticleSystem>();   
+        dustParticles = dust.GetComponent<ParticleSystem>();
         stoneParticles = stoneEmitter.GetComponent<ParticleSystem>();
 
-
+        timeToDeactivate = trailParticles.main.startLifetime.constant;
     }
 
-	// Use this for initialization
-	void Start () {       
+    // Use this for initialization
+    void Start()
+    {
 
         trailParticles.Play();
         dustParticles.Stop();
         stoneParticles.Stop();
+        Invoke("StopAttack", lifeTime);
     }
-	
-	// Update is called once per frame
-	void Update () {
 
-       if(!stop)
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!stop)
         {
             MoveParticleSystem(moveDirection);
-        }    
-	}
+        }
+    }
 
     void MoveParticleSystem(Vector3 direction)
     {
@@ -50,15 +57,34 @@ public class WildBoar : MonoBehaviour {
 
     void OnTriggerEnter(Collider other)
     {
-
-        trailParticles.Stop();
-        dustParticles.Play();
-        stoneParticles.Play();
-        stop = true;
+        if (LayerMask.LayerToName(other.gameObject.layer) == "destroyableWildboar")
+        {
+            trailParticles.Stop();
+            dustParticles.Play();
+            stoneParticles.Play();
+            stop = true;
+            collision = true;
+            other.gameObject.SendMessage("ApplyDamage", 0, SendMessageOptions.DontRequireReceiver);
+        }
     }
 
     public void SetMoveDirection(Vector3 direction)
     {
         moveDirection = direction;
+    }
+
+    void StopAttack()
+    {
+        if (!collision)
+        {
+            trailParticles.Stop();
+            stop = true;
+            Invoke("DisableWildboar", timeToDeactivate);
+        }
+    }
+
+    void DisableWildboar()
+    {
+        gameObject.SetActive(false);
     }
 }
