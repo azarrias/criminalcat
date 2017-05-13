1d2ab9512915636d31513b7e97c3d01841ee831e
azarrias <adolfo.zarrias@gmail.com>
2017-05-13 21:58:17 +0200

    Add colliders to viking enemy prefab

---
+++ Metalhalla/Assets/Scripts/Enemies shared scripts/EnemyAttackColliderBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttackColliderBehaviour : MonoBehaviour
+{
+
+    public LayerMask hittableLayer;
+
+    private void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (hittableLayer == (hittableLayer | (1 << other.gameObject.layer)))
+        {
+            other.gameObject.SendMessage("ApplyDamage", 5, SendMessageOptions.DontRequireReceiver);
+        }
+    }
+}
