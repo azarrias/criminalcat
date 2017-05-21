0ae0fef66f5645ecfb9b28e2e56cdc47cc7a76cb
azarrias <adolfo.zarrias@gmail.com>
2017-05-21 21:42:19 +0200

    Create object pool manager

---
+++ Metalhalla/Assets/ObjectPoolManagerScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPoolManagerScript : MonoBehaviour {
+
+    public static ObjectPoolManagerScript instance;
+    public GameObject pooledObject;
+    public int pooledAmount = 20;
+
+    List<GameObject> pooledObjects;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start() {
+        pooledObjects = new List<GameObject>();
+        for(int i = 0; i < pooledAmount; ++i)
+        {
+            GameObject obj = (GameObject)Instantiate(pooledObject);
+            obj.SetActive(false);
+            pooledObjects.Add(obj);
+        }
+	}
+
+    public GameObject GetPooledObject()
+    {
+        for(int i = 0; i < pooledObjects.Count; ++i)
+        {
+            if(!pooledObjects[i].activeInHierarchy)
+            {
+                return pooledObjects[i];
+            }
+        }
+
+        GameObject obj = (GameObject)Instantiate(pooledObject);
+        pooledObjects.Add(obj);
+        return obj;
+    }
+	
+}
