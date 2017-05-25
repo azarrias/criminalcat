b1f187d57541d200cc21ce6f746e3a6b2d0cd870
eikur <jorge.soriano.pinedo@gmail.com>
2017-05-25 17:41:54 +0200

    Fixed limit cases in Boss' lair layers and added random rotation start and speed to coinItem script

--- Metalhalla/Assets/Scripts/Miscellaneous scripts/coinItem.cs
+++ Metalhalla/Assets/Scripts/Miscellaneous scripts/coinItem.cs
@@ -4,11 +4,23 @@ using UnityEngine;
 
 public class coinItem : MonoBehaviour {
 
+    private float rotationSpeed;
+    private void Start()
+    {
+        transform.Rotate(Random.Range(0, 90) * Vector3.up);
+        rotationSpeed = Random.Range(100, 150);
+    }
+    private void Update()
+    {
+        transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+    }
+
     private void OnTriggerEnter(Collider collision)
     {
         if (collision.tag == "Player")
         {
             // add score / items to player
+            // play sound here
             Destroy(this.gameObject);
         }
     }
