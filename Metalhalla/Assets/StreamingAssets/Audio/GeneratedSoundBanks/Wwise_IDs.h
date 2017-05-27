09329c9292d84672bcd4a993ebcc2352ea71c435
azarrias <adolfo.zarrias@gmail.com>
2017-05-27 11:41:27 +0200

    First audio test with Wwise

--- Metalhalla/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Metalhalla/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -11,6 +11,11 @@
 
 namespace AK
 {
+    namespace EVENTS
+    {
+        static const AkUniqueID FOOTSTEP = 1866025847U;
+    } // namespace EVENTS
+
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
