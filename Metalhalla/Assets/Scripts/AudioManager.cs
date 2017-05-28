b59ee8387016860f129134b609840c5eb6f13617
azarrias <adolfo.zarrias@gmail.com>
2017-05-28 13:06:42 +0200

    Uninstall wwise integration of hell and add AudioManager

---
+++ Metalhalla/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioManager : MonoBehaviour {
+
+    AudioSource audioSource;
+
+    [Header("Music")]
+    public AudioClip introCutscene;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        audioSource = GetComponent<AudioSource>();
+    }
+
+	// Use this for initialization
+	void Start () {
+        audioSource.clip = introCutscene;
+        audioSource.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
