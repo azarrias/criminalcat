bda77c8def43c26f1f92c994c4e79192ba4611e2
eikur <jorge.soriano.pinedo@gmail.com>
2017-06-16 13:07:39 +0200

    Updated death fade out with custom message and balanced the times for a longer out and a shorter return to scene

--- Metalhalla/Assets/Scripts/Camera Scripts/CameraFade.cs
+++ Metalhalla/Assets/Scripts/Camera Scripts/CameraFade.cs
@@ -5,32 +5,54 @@ using UnityEngine;
 public class CameraFade : MonoBehaviour {
 
     public Texture2D fadeTexture;
-    public float fadeTime = 6.0f;
+    public Texture2D deadImage;
+    public float fadeToBlackTime = 3.0f;
+    public float showImageStartTime = 1.0f;
+    public float stayInBlackTime = 2.0f;
+    public float fadeToSceneTime = 1.0f;
+
+    float fadeTime;
 
     [HideInInspector]
     private bool isFading = false;
+    private bool showMessage = false; 
     private float fadeTimeCurrent = 0.0f;
 
     private Color guiColor;
 
 	void Start () {
+        guiColor = Color.white;
         guiColor.a = 0.0f;
+        fadeTime = fadeToBlackTime + stayInBlackTime+ fadeToSceneTime;
     }
 	
 	void Update () {
+        // DEBUG
         if (Input.GetKeyDown(KeyCode.R) == true && isFading == false)
             ActivateFade();
+
         if ( isFading == true )
         {
             if (fadeTimeCurrent >= fadeTime)
             {
                 guiColor.a = 0.0f;
                 isFading = false;
-
             }
             else
             {
-                guiColor.a = 1 - Mathf.Abs((fadeTime * 0.5f - fadeTimeCurrent) / (fadeTime * 0.5f));
+                if ( fadeTimeCurrent <= fadeToBlackTime)
+                {
+                    if (fadeTimeCurrent >= showImageStartTime)
+                    {
+                        showMessage = true; 
+                    }
+                    guiColor.a = 1 - (fadeToBlackTime - fadeTimeCurrent) / fadeToBlackTime;
+                }
+                else if (fadeTimeCurrent >= fadeToBlackTime + stayInBlackTime)
+                {
+                    showMessage = false;
+                    guiColor.a = (fadeToSceneTime - fadeTimeCurrent + fadeToBlackTime + stayInBlackTime) / fadeToSceneTime;
+                }
                 fadeTimeCurrent += Time.deltaTime;
             }
         }
@@ -43,7 +65,10 @@ public class CameraFade : MonoBehaviour {
         {
             GUI.color = guiColor;
             GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), fadeTexture);
+            if (showMessage == true)
+                GUI.DrawTexture(new Rect(0, Screen.height / 4, Screen.width, Screen.height / 4), deadImage);
         }
+        
     }
 
     public void ActivateFade()
