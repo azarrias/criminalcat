f2e7adff33d1880e066b67e49d10ef3dd9d78e44
eikur <jorge.soriano.pinedo@gmail.com>
2017-06-22 17:12:36 +0200

    Added hit fx to player's attack collider when hitting an enemy and updated prefabs in both scenes

--- Metalhalla/Assets/Scripts/Player Class/PlayerAttackColliderBehaviour.cs
+++ Metalhalla/Assets/Scripts/Player Class/PlayerAttackColliderBehaviour.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerAttackColliderBehaviour : MonoBehaviour {
 
     public LayerMask hittableLayer;
+    public AudioClip fxHit;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -12,6 +13,8 @@ public class PlayerAttackColliderBehaviour : MonoBehaviour {
         {
             // todo: update with modifiable damage
             other.gameObject.SendMessage("ApplyDamage", 10, SendMessageOptions.DontRequireReceiver);
+            if (fxHit && (other.CompareTag("Viking") || other.CompareTag("Dark Elf") || other.CompareTag("Boss")) )
+                AudioManager.instance.PlayFx(fxHit);
         }
     }
 }
