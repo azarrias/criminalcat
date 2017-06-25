2a662c7d2265d1151cdbf4a3f21f071914852f4d
Daniel Pérez <dperezparaiso@gmail.com>
2017-06-25 21:00:53 +0200

    Added fadeout and dust effect to rockfall in corridor

--- Metalhalla/Assets/Scripts/Miscellaneous scripts/RockBehaviour.cs
+++ Metalhalla/Assets/Scripts/Miscellaneous scripts/RockBehaviour.cs
@@ -6,25 +6,64 @@ public class RockBehaviour : MonoBehaviour {
 
     public int damage = 10;
     public float lifeTime = 3.0f;
-    //ParticleSystem rockDustParticles;
+    private ParticleSystem rockDustParticles;
+    public bool disipate = false;
+    public float disipationTime = 2.0f;
+    private float disipationCounter = 0.0f;    
+    private MeshRenderer estalactiteMR;
+    public float fadeoutSpeed = 0.01f;
 
-    
-    void OnEnable()
+    void Start()
     {
-        //rockDustParticles.Play();
+        rockDustParticles = GetComponent<ParticleSystem>();
+        GameObject estalactiteMeshGO = transform.Find("estalactita/Group6554").gameObject;
+        estalactiteMR = estalactiteMeshGO.GetComponent<MeshRenderer>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (disipate)
+            Disipate();
     }
 
     void OnTriggerEnter(Collider collider)
     {       
-        if(collider.CompareTag("Player"))
+        if(collider.CompareTag("Player") && !disipate)
         {
             collider.gameObject.SendMessage("ApplyDamage", damage);
         }
     }
 
-    private void DestroyRock()
+    private void Disipate()
     {
-        //rockDustParticles.Stop();
-        gameObject.SetActive(false);
+        if (disipationCounter == 0.0f)
+        {
+            rockDustParticles.Play();
+            gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            gameObject.GetComponent<Rigidbody>().useGravity = false;
+        }
+
+        //fadeout
+        Color color = estalactiteMR.materials[0].color;
+
+        color.a -= fadeoutSpeed;
+        if (color.a < 0.0f)
+            color.a = 0.0f;
+
+        estalactiteMR.materials[0].color = color;
+
+        disipationCounter += Time.deltaTime;
+        if(disipationCounter >= disipationTime)
+        {
+            disipationCounter = 0.0f;
+            disipate = false;
+            gameObject.SetActive(false);
+            gameObject.GetComponent<Rigidbody>().useGravity = true;
+            Color startingColor = estalactiteMR.materials[0].color;
+            startingColor.a = 1.0f;
+            estalactiteMR.materials[0].color = startingColor;
+        }
     }
 }
