da01ac2122583d380f8c52d74e795285d55707c2
eikur <jorge.soriano.pinedo@gmail.com>
2017-08-22 23:44:43 +0200

    Created a RoutePlanner prefab to set directly in the editor the cave bats' routes

---
+++ Metalhalla/Assets/GuidedRoute.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuidedRoute : MonoBehaviour {
+    [Tooltip("Gameobject that will follow the route")]
+    public GameObject mc;
+
+    [Header("Parameters")]
+    [Tooltip("Route points that define the route the MC gameobject will follow. Should have at least 2 points")]
+    public Transform[] stepPoints;
+
+    [Tooltip("Determines if the mc will travel from the last point to the first or not")]
+    public bool loop = true;
+
+    [Tooltip("Speed to move in the route")]
+    public float speed = 2.0f;
+
+    private int totalSteps;
+    private int currentStepIndex;
+    private int nextStepIndex;
+    private Vector3 currentStep;
+    private Vector3 nextStep;
+    private Vector3 currentDirection;
+    private float currentDistance;
+    private bool reverse = false;
+
+    private void Start()
+    {
+        totalSteps = stepPoints.Length;
+        if (totalSteps >= 2)
+        {
+            currentStepIndex = 0;
+            nextStepIndex = 1;
+            UpdateSteps();
+            UpdateMCRotation();
+            mc.transform.position = transform.position;
+        }
+        else
+        {
+            Debug.LogError("Guided route step points aren't properly setup (there's not at least 2 of those).");
+        }
+    }
+
+	void Update () {
+        //mc.transform.Translate(currentDirection * Time.deltaTime * speed);
+        mc.transform.position += currentDirection * speed * Time.deltaTime;
+        if (Vector3.Distance(currentStep, mc.transform.position) >= currentDistance)
+        {
+            UpdateStepIndexes();
+            UpdateSteps();
+            UpdateMCRotation();
+        }
+	}
+
+    private void UpdateStepIndexes()
+    {
+        if (loop == true)
+        {
+            currentStepIndex = nextStepIndex;
+            nextStepIndex = (nextStepIndex + 1) % totalSteps;
+        }
+        else
+        {
+            
+            currentStepIndex = nextStepIndex;
+            nextStepIndex += reverse ? -1 : 1; 
+            if (nextStepIndex < 0)
+            {
+                nextStepIndex = 1;
+                reverse = false;
+            }
+            else if (nextStepIndex == totalSteps)
+            {
+                nextStepIndex = totalSteps - 2;
+                reverse = true; 
+            }
+        }
+    }
+
+    private void UpdateSteps()
+    {
+        currentStep = stepPoints[currentStepIndex].position;
+        nextStep = stepPoints[nextStepIndex].position;
+        currentDirection = (nextStep - currentStep).normalized;
+        currentDistance = (nextStep - currentStep).magnitude;
+    }
+
+    private void UpdateMCRotation()
+    {
+        mc.transform.rotation = Quaternion.LookRotation(currentDirection);
+    } 
+}
