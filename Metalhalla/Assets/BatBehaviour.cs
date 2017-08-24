f03576c630f5bfc924289668a5c3cc7713995b48
azarrias <adolfo.zarrias@gmail.com>
2017-08-25 00:15:35 +0200

    Add bat flying flapping wings sound fx

---
+++ Metalhalla/Assets/BatBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatBehaviour : MonoBehaviour {
+
+    [Header("Sound Effects")]
+    public AudioClip[] fxFlapWings;
+
+    public void FlapWings()
+    {
+        AudioManager.instance.RandomizePlayFx(fxFlapWings);
+    }
+}
