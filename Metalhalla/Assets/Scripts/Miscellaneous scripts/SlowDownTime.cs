fd7d7c711bfb7b0ac7c7bf3845080bf272df52b6
eikur <jorge.soriano.pinedo@gmail.com>
2017-09-23 12:27:26 +0200

    Added slow motion to end cutscene and timeout to change to credits scene

---
+++ Metalhalla/Assets/Scripts/Miscellaneous scripts/SlowDownTime.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowDownTime : MonoBehaviour {
+
+    public float slowedTimeScale = 0.2f;
+    public string newSceneName = "Credits";
+    public float changeToSceneIn = 1.8f;
+
+    private float elapsedTime = 0.0f;
+    private bool startCountdown = false;
+    private SceneLoader loader;
+
+    private float previousTimeScale;
+    private float previousFixedDeltaTime;
+
+    void Start()
+    {
+        GetComponent<Renderer>().enabled = false;
+        loader = GameObject.FindWithTag("SceneLoader").GetComponent<SceneLoader>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (startCountdown )
+        {
+            elapsedTime += Time.fixedDeltaTime;
+            if (elapsedTime >= changeToSceneIn)
+            {
+                loader.GoToNextScene(newSceneName);
+                Time.timeScale = previousTimeScale;
+                Time.fixedDeltaTime = previousFixedDeltaTime;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.tag == "Player")
+        {
+            previousTimeScale = Time.timeScale;
+            previousFixedDeltaTime = Time.fixedDeltaTime;
+
+            Time.timeScale = slowedTimeScale;
+            Time.fixedDeltaTime *= slowedTimeScale;
+            elapsedTime = 0.0f;
+            startCountdown = true;
+        }
+    }
+}
