5573a559f77604f224ea5d6a9221df4ec8f18c0b
eikur <jorge.soriano.pinedo@gmail.com>
2017-09-25 12:14:30 +0200

    Split platform stay trigger into stay and leave triggers to prevent the player being parented to a liftable platform when it should not - and going through the floor

---
+++ Metalhalla/Assets/Scripts/Platforms Scripts/PlatformLeaveTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformLeaveTrigger : MonoBehaviour {
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            PlayerStatus playerStatus = other.GetComponent<PlayerStatus>();
+            //if (playerStatus.IsJump() == true || playerStatus.IsDash() || playerStatus.IsFall() || playerStatus.IsWalk())
+                other.gameObject.transform.parent = null;
+        }
+    }
+}
